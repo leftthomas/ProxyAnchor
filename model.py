31f0b3455d20cfd106958f317d7f34fad7ee5436
leftthomas <leftthomas@qq.com>
2020-07-14 18:16:03 +0800

    use var to replace std

--- model.py
+++ model.py
@@ -17,8 +17,8 @@ class ProxyLinear(nn.Module):
     def forward(self, x):
         normalized_weight = F.normalize(self.weight, dim=-1)
         if not self.use_temperature:
-            std = torch.std(normalized_weight, dim=0, unbiased=False, keepdim=True)
-            normalized_weight = normalized_weight / std
+            var = torch.var(normalized_weight, dim=0, unbiased=False, keepdim=True)
+            normalized_weight = normalized_weight / torch.sqrt(var)
         output = x.matmul(normalized_weight.t())
         return output
 
