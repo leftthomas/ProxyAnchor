61ad75d14426fa5dc183c2b3b238b2c8da35982a
leftthomas <leftthomas@qq.com>
2020-07-14 14:29:15 +0800

    fix bug about plt

--- toy.py
+++ toy.py
@@ -1,6 +1,7 @@
 import argparse
 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import torch
 import torch.nn as nn
@@ -119,15 +120,15 @@ def for_loop(net, mode=True):
     density_mean /= len(density_list)
     desc += 'D@Mean:{:.2f} '.format(density_mean)
     print(desc)
-    # TODO
-    # plot(embed, label, fig_path='results/baseline.png')
+    plot(embeds.cpu().numpy(), outputs,
+         fig_path='results/{}_{}_{}.png'.format('Train' if mode else 'Test', epoch, temperature))
 
 
-def plot(embeds, labels, fig_path='./example.pdf'):
+def plot(embeds, labels, fig_path):
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
 
-    # Create a sphere
+    # create a sphere
     r = 1
     pi = np.pi
     cos = np.cos
@@ -136,14 +137,12 @@ def plot(embeds, labels, fig_path='./example.pdf'):
     x = r * sin(phi) * cos(theta)
     y = r * sin(phi) * sin(theta)
     z = r * cos(phi)
-    ax.plot_surface(
-        x, y, z, rstride=1, cstride=1, color='w', alpha=0.3, linewidth=0)
+    ax.plot_surface(x, y, z, rstride=1, cstride=1, color='w', alpha=0.3, linewidth=0)
     ax.scatter(embeds[:, 0], embeds[:, 1], embeds[:, 2], c=labels, s=20)
 
     ax.set_xlim([-1, 1])
     ax.set_ylim([-1, 1])
     ax.set_zlim([-1, 1])
-    ax.set_aspect("equal")
     plt.tight_layout()
     plt.savefig(fig_path)
 
